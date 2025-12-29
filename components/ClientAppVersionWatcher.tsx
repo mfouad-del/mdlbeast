2f0b0ca419b0ff3645a7631d5bc123b538c1f7cd
Mahmoud-Fouad2 <mahmoud.a.fouad2@gmail.com>
2025-12-29 12:01:57 +0300

    chore(reload): add silent auto-reloader; remove UpdateBanner and AppVersionWatcher files per request

--- components/ClientAppVersionWatcher.tsx
+++
@@ -1,7 +0,0 @@
-"use client"
-import React from 'react'
-import AppVersionWatcher from './AppVersionWatcher'
-
-export default function ClientAppVersionWatcher() {
-  return <AppVersionWatcher />
-}
